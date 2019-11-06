26f856ad31349c2f13cb9e6001d35ddb87fdc468
Zack Carlson <zackcarlson@IAB-MAC-00012.local>
2019-11-05 18:03:42 -0800

    Style publisher page

--- src/docs/components/examples/pub-integration/publisher.jsx
+++ src/docs/components/examples/pub-integration/publisher.jsx
@@ -1,12 +1,55 @@
 import { h, Component } from 'preact';
+import style from './publisher.less'; 
+import image from './public/images/capital.jpg';
+import React from 'react';
+import { Link } from 'react-router-dom'; 
 
 export default class Publisher extends Component { 
     render() {
         return (
             <div>
-                <h1>Publisher Daily</h1>
-                <h4>Latest News</h4>
-                <p>This is the home page for the Publisher.</p>
+                <nav>
+                    <header>
+                        <h1>Publisher Daily</h1>
+                    </header>
+                    <nav className={style.subMenu}>
+                        <ul>
+                            <li>News</li>
+                            <li>Politics</li>
+                            <li>Entertainment</li>
+                            <li>Life</li>
+                        </ul>
+                    </nav>
+                </nav>
+
+                <main>
+                    <div>
+                        <ul>
+                            <h4>#Trending</h4>
+                            <li>Whistleblower</li>
+                            <li>FDA Recall</li>
+                            <li>Water Crisis</li>
+                            <li>Looming Crash</li>
+                            <li>Racey Humor</li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <div className={style.frontPage}>
+                            <span>Congressional Trial Underway</span>
+                            <div className={style.frontPagePhoto}>
+                                <img src={image}/>
+                            </div>
+                        </div>
+                    </div>
+
+                    <Link to='/cmp'>
+                        <button className={style.consentButton}>
+                            Update Consent
+                        </button>
+                    </Link>
+                </main>
+                
             </div>
         );
     }
